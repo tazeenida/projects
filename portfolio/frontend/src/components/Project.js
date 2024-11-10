a494a80bf84400bd209ca7dba25beff90cf7e20e
Nida <tazen01@pfw.edu>
2024-11-10 18:40:17 -0500

    updated project

--- portfolio/frontend/src/components/Project.js
+++ portfolio/frontend/src/components/Project.js
@@ -1,17 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
-import axios from 'axios';
-import '../App.css'
-
-const backendUrl = 'https://projects-yybm.onrender.com';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function Project() {
   return (
-    <iframe
-      src="https://bookrecommendationsystem-qf5o.onrender.com/"
-      style={{ width: '100%', height: '100vh', border: 'none' }}
-      title="App1"
-    />
+    <Router>
+      <Routes>
+        <Route path="/bookrecommendationsystem" element={<Navigate to="https://bookrecommendationsystem-qf5o.onrender.com/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
