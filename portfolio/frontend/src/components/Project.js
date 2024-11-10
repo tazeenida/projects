3c109d5d867892221d2760445c8c3cae4e38748d
Nida <tazen01@pfw.edu>
2024-11-10 18:46:05 -0500

    updated project

--- portfolio/frontend/src/components/Project.js
+++ portfolio/frontend/src/components/Project.js
@@ -1,13 +1,11 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function Project() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/bookrecommendationsystem" element={<Navigate to="https://bookrecommendationsystem-qf5o.onrender.com/" replace />} />
-      </Routes>
-    </Router>
-  );
+  useEffect(() => {
+    window.location.href = "https://bookrecommendationsystem-qf5o.onrender.com/";
+  }, []);
+
+  return null;
 }
 
 export default Project;
